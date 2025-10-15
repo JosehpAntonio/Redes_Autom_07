72eb524780cc1df912ecd0362f9c3ce961b60e7a
JosephCG <75400350@iegb.edu.pe>
2025-10-15 17:24:25 -0500

    Agregar Mensaje en Rama de Desarollo

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-def saludar(nombre):
+def saludar():
+    nombre = input("Ingresa tu nombre: ")
     print(f"Hola, {nombre}! Bienvenido al proyecto colaborativo")
 
 if __name__ == "__main__":
-    saludar("Joseph")
+    saludar()
+    
